e36659b74bab46dca9b8eb3c97acc090f560899a
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-08 23:49:20 -0700

    Multiple handle things blocking construction

---
+++ Source/HandleAllBlockingThings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using static System.Reflection.Emit.OpCodes;
+using Harmony;
+using Verse;
+using Verse.AI;
+using RimWorld;
+
+namespace Share_The_Load
+{
+	[HarmonyPatch(typeof(GenConstruct), "HandleBlockingThingJob")]
+	class HandleAllBlockingThings
+	{
+		//public static Job HandleBlockingThingJob(Thing constructible, Pawn worker, bool forced = false)
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo FirstBlockingThingInfo = AccessTools.Method(typeof(GenConstruct), "FirstBlockingThing");
+
+			MethodInfo FirstReservableBlockingThingInfo = AccessTools.Method(typeof(HandleAllBlockingThings), nameof(FirstReservableBlockingThing));
+
+			foreach (CodeInstruction i in instructions)
+			{
+				if (i.opcode == Call && i.operand == FirstBlockingThingInfo)
+					i.operand = FirstReservableBlockingThingInfo;
+				yield return i;
+			}
+		}
+
+		public static Thing FirstReservableBlockingThing(Thing constructible, Pawn pawnToIgnore)
+		{
+			Thing thing = constructible is Blueprint b ? GenConstruct.MiniToInstallOrBuildingToReinstall(b) : null;
+
+			for(var iterator = constructible.OccupiedRect().GetIterator(); !iterator.Done(); iterator.MoveNext())
+				foreach(Thing t in iterator.Current.GetThingList(constructible.Map))
+					if (GenConstruct.BlocksConstruction(constructible, t) && t != pawnToIgnore && t != thing && pawnToIgnore.CanReserve(t))
+						return t;
+
+			return null;
+		}
+	}
+}
