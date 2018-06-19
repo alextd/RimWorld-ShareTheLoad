36d57eba7b04c1cd0c342c082e057f4e74c66ffb
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-06-18 22:48:15 -0700

    Fix up for 1.0

--- Source/DeliverAsMuchAsPossible.cs
+++ Source/DeliverAsMuchAsPossible.cs
@@ -30,13 +30,12 @@ namespace Share_The_Load
 	//Honestly is a vanilla bug
 	//Would only deliver resource #2 once there's enough resource #1 
 	//Though resource #1 doesn't care if there's enough #2
-	//[HarmonyPatch(typeof(WorkGiver_ConstructDeliverResources), "ResourceDeliverJobFor")]
+	[HarmonyPatch(typeof(WorkGiver_ConstructDeliverResources), "ResourceDeliverJobFor")]
 	public static class BreakToContinue_Patch
 	{
 		//protected Job ResourceDeliverJobFor(Pawn pawn, IConstructible c, bool canRemoveExistingFloorUnderNearbyNeeders = true)
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
 		{
-			Label forCheck = new Label();
 			Label continueLabel = il.DefineLabel();
 			bool setLabel = false;
 
@@ -45,23 +44,23 @@ namespace Share_The_Load
 			{
 				CodeInstruction inst = instList[i];
 
-				//i=0;Br To Check label
-				if (inst.opcode == OpCodes.Br && instList[i - 1].opcode == OpCodes.Stloc_S && instList[i - 2].opcode == OpCodes.Ldc_I4_0)
+				//i=0;Br To for condition
+				if (!setLabel && inst.opcode == OpCodes.Br && instList[i - 1].opcode == OpCodes.Stloc_S && instList[i - 2].opcode == OpCodes.Ldc_I4_0)
 				{
-					forCheck = (Label)inst.operand;
-
-					List<CodeInstruction> instructionsList = instructions.ToList();
-					for (int k = instructionsList.Count() - 1; k >= 0; k--)
+					Label forCheck = (Label)inst.operand;
+					
+					for (int k = instList.Count() - 1; k >= 4; k--)
 					{
-						if (instructionsList[k].labels.Contains(forCheck))
+						if (instList[k].labels.Contains(forCheck))
 						{
-							instructionsList[k - 4].labels.Add(continueLabel);
+							instList[k - 4].labels.Add(continueLabel);
 							setLabel = true;
 							break;
 						}
 					}
 				}
-				else if (inst.opcode == OpCodes.Br && instList[i - 2].opcode == OpCodes.Ldfld)// operand == need, but inside a compilergenerated mess
+				//break; preceded by thingDefCountClass = object.need;
+				else if (inst.opcode == OpCodes.Br && instList[i - 1].opcode == OpCodes.Stloc_S && instList[i - 2].opcode == OpCodes.Ldfld)// operand == need, but inside a compilergenerated mess
 				{
 					if (setLabel)
 						inst.operand = continueLabel;
